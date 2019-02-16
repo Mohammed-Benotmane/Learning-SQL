2d3d14748b4c0f3f49968e8e8ecfaa3467d1383c
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2019-02-16 01:15:09 +0100

    modified names and refactor code

--- test.sql
+++ test.sql
@@ -1,19 +1,24 @@
 show databases;
 create database BD;
-show databases;
+use BD;
 create table Auteurs(NoAuteur int, Nom_auteur varchar(50), mail varchar(50),primary key(NoAuteur));
 create table Editeur(NoEditeur int, Nom_Editeur varchar(50), adresse varchar(50), telephone varchar(20), primary key(NoEditeur));
 create table Livre(ID_livre int,titre varchar(50),Nbr_exemplaire int,NoEditeur int,primary key(ID_livre),foreign key(NoEditeur)references Editeur(NoEditeur));
 create table Ecritpar(ID_livre int,NoAuteur int, primary key(ID_livre,NoAuteur),foreign key(ID_livre)references Livre(ID_livre),foreign key(NoAuteur)references Auteurs(NoAuteur));
-insert into Editeur(NoEditeur,Nom_Editeur,adresse,telephone) values (1,"Mohammed","yaghmoracen","066666666");
-insert into Editeur(NoEditeur,Nom_Editeur,adresse,telephone) values (2,"Hafid","Mosta","07777777");
-insert into Editeur(NoEditeur,Nom_Editeur,adresse,telephone) values (3,"Feriel","mdin jdida","088888888");
+
+insert into Editeur(NoEditeur,Nom_Editeur,adresse,telephone) 
+values (1,"Editeur1","yaghmoracen","066666666");
+insert into Editeur(NoEditeur,Nom_Editeur,adresse,telephone) 
+values (2,"Editeur2","Mosta","07777777");
+insert into Editeur(NoEditeur,Nom_Editeur,adresse,telephone) 
+values (3,"Editeur3","mdin jdida","088888888");
+
 insert into Auteurs(NoAuteur,Nom_auteur,mail) 
-values (1,"Benotmane","yaghmoracen@moh.com");
+values (1,"Auteur1","yaghmoracen@moh.com");
 insert into Auteurs(NoAuteur,Nom_auteur,mail) 
-values (2,"Bouhelalene","mosta@moh.com");
+values (2,"Auteur2","mosta@moh.com");
 insert into Auteurs(NoAuteur,Nom_auteur,mail) 
-values (3,"Fatmi","mdinjdida@moh.com");
+values (3,"Auteur3","mdinjdida@moh.com");
 
 insert into Livre(ID_livre,titre,Nbr_exemplaire,NoEditeur)
 values(1,"Harry Potter",15,2);
